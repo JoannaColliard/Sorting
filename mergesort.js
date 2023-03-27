e472f48f1f7083451ad76651de68c180e9641b86
JoannaColliard <88058940+JoannaColliard@users.noreply.github.com>
2023-03-27 19:06:49 -0300

    Avance workshop sorting II

--- mergesort.js
+++ mergesort.js
@@ -3,10 +3,35 @@ function split(wholeArray) {
     if (wholeArray.length <= 1) {
         return wholeArray;
     } else {
-        let half = Math.floor(wholeArray.length / 2);  //["a", "b", "c", "d", "e"]
+        let half = Math.floor(wholeArray.length / 2);  //[6, 2, 20, 1, 5]
         let firstHalf = wholeArray.slice(0, half);
         let secondHalf = wholeArray.slice(half);
 
         return [firstHalf, secondHalf];
     }
 }
+
+function merge(firstHalf, secondHalf) {
+
+}
+
+function mergeSort(array) {
+
+    if (array.length === 1) {
+        return array;
+    } else {
+        let arr = split(array);
+        let left = arr[0];
+        let right = arr[1];
+        return array;
+    }
+
+}
+
+/*
+mergeSort recibe un arreglo desordenado y devuelve el mismo arreglo ordenado.
+
+split recibe un arreglo y lo divide en dos partes que pueden ser iguales o no, dependiendo de la cantidad de elementos que tenga el arreglo. 
+
+merge toma las dos partes del arreglo que divide split y las une en un solo arreglo ordenado.
+*/
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,15 +1,20 @@
-describe('Split Array function', function() {
-    it('es capaz de dividir el arreglo en dos partes', function() {
-      expect( split([1, 2, 3, 4, 5]) ).toEqual( [[1, 2], [3, 4, 5]] );
-      expect( split([1, 2, 3, 4]) ).toEqual( [[1, 2], [3, 4]] );
+describe('Split Array function', function () {
+    it('es capaz de dividir el arreglo en dos partes', function () {
+        expect(split([1, 2, 3, 4, 5])).toEqual([[1, 2], [3, 4, 5]]);
+        expect(split([1, 2, 3, 4])).toEqual([[1, 2], [3, 4]]);
     });
 
-    it('si recibe un arreglo vacío, devuelve un arreglo vacío', function() {
-      expect( split([]) ).toEqual( [] );
+    it('si recibe un arreglo vacío, devuelve un arreglo vacío', function () {
+        expect(split([])).toEqual([]);
     });
 
-    it('si recibe un arreglo con un solo elemento, devuelve el mismo arreglo', function() {
-      expect( split([1]) ).toEqual( [1] );
+    it('si recibe un arreglo con un solo elemento, devuelve el mismo arreglo', function () {
+        expect(split([1])).toEqual([1]);
     });
+});
 
-  });
+describe('Merge', function () {
+    it('es capaz de combinar dos Arreglos ordenados en uno solo ordenado', function () {
+        // testeá el algoritmo
+    });
+});
